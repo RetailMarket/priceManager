da08b2c3deb9fbd28289bbca4d074ad82bbc8988
Akshay <kumarsharmaa69@gmail.com>
2017-01-20 12:26:16 +0530

    modified routers

--- routes/router.go
+++ routes/router.go
@@ -1,32 +1,16 @@
 package routes
 
 import (
-	"fmt"
-	"strconv"
 	"github.com/gin-gonic/gin"
+	"database/sql"
+	"Retail/priceManager/handler"
 )
 
-func HandleRequest() {
+func HandleRequest(db *sql.DB) {
 	router := gin.Default()
 
-	router.GET("/price/update", saveUpdatedPrice)
+	router.POST("/price/update", handler.SaveUpdatePrice(db))
 
 	router.Run(":4000")
 }
 
-func saveUpdatedPrice(c *gin.Context) {
-	name := c.Query("name")
-	price := parsePrice(c)
-
-	// can store price to price update table
-	fmt.Printf("updating %s price to %.2f\n", name, price)
-}
-
-func parsePrice(c *gin.Context) float64 {
-	price, err := strconv.ParseFloat(c.Query("price"), 64)
-
-	if (err != nil) {
-		panic(fmt.Sprintf("Given price %s is wrong", c.Query("price")))
-	}
-	return price
-}
