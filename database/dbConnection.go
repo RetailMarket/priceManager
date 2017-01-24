6aedb8ce24e79921b51ea7b25f19b53dc0e792ce
Akshay <kumarsharmaa69@gmail.com>
2017-01-24 10:18:37 +0530

    Made db object accessible at all places, no need to create again and again and also create methods for changing the status

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -11,40 +11,59 @@ import (
 const (
 	DB_DRIVER = "postgres"
 	DB_CONNECTION = "user=postgres dbname=postgres password=postgres sslmode=disable"
+	SCHEMA_NAME = "price"
+	PRICE_UPDATE_REQUEST_TABLE_NAME = "priceUpdateRequest"
+	PRICE_TABLE_NAME = "price"
 )
 
-func OpenDatabase() *sql.DB {
-	db, err := sql.Open(DB_DRIVER, DB_CONNECTION)
+var db *sql.DB;
+
+func Init() {
+	var err error;
+	db, err = sql.Open(DB_DRIVER, DB_CONNECTION)
 	if (err != nil) {
 		log.Fatal(err.Error())
 	}
 	db.Ping()
+}
+
+func GetDb() *sql.DB {
 	return db;
 }
 
-func SavePriceInUpdateTable(db *sql.DB, priceObj *model.Price) {
-	fmt.Println("Inserting record into updatePriceRequest table...")
-	insertQuery := "insert into price.updatePriceRequest (product_id, product_name,cost,status) values($1,$2,$3,$4)"
+func CloseDb() {
+	db.Close()
+}
 
-	_, err := db.Exec(
-		insertQuery,
+func SavePriceInUpdateTable(priceObj *model.Product) {
+	tx, err := db.Begin();
+	if err != nil {
+		log.Fatalf("Unable to create transection, error while saving update price request to db\n Error %v", err);
+	}
+	fmt.Println("Inserting record into priceUpdateRequest table...")
+	insertQuery := fmt.Sprintf("insert into %s.%s (product_id, product_name,cost,status) values(%s,%s,%s,%s)", SCHEMA_NAME,
+		PRICE_UPDATE_REQUEST_TABLE_NAME,
 		priceObj.Product_id,
 		priceObj.Product_name,
 		priceObj.Cost,
 		priceObj.Status)
 
+	_, err = db.Exec(insertQuery)
+
 	if (err != nil) {
+		tx.Rollback();
 		log.Fatalf("Unable to save update entry in db \nError: %v", err.Error());
+	} else {
+		tx.Commit();
 	}
 }
 
-func ValidateEntryPresence(db *sql.DB, id int) {
-	selectRecordCountByIdQuery := "select count(*) from price.price where product_id = $1"
-	record, err := db.Query(selectRecordCountByIdQuery, id)
-	if(err != nil){
-		log.Fatal(err)
+func ValidateEntryPresence(id int) {
+	selectRecordCountByIdQuery := fmt.Sprintf("select count(*) from %s.%s where product_id = %s", SCHEMA_NAME, PRICE_TABLE_NAME, id);
+	record, err := db.Query(selectRecordCountByIdQuery)
+	if (err != nil) {
+		log.Fatalf("Query failed while validating product id \n err: %v", err)
 	}
-
 	next := record.Next()
 
 	if !next {
@@ -52,10 +71,20 @@ func ValidateEntryPresence(db *sql.DB, id int) {
 	}
 }
 
-func CleanUpUpdatePriceRequestTable(db *sql.DB) {
-	truncateQuery := "truncate table price.updatePriceRequest"
-	_, err := db.Exec(truncateQuery)
+func GetUpdateRequestEntriesWithStatus(status string, tx sql.Tx) *sql.Rows {
+	selectQuery := fmt.Sprintf("select product_id, product_name, cost from %s.%s where status='%s'", SCHEMA_NAME, PRICE_UPDATE_REQUEST_TABLE_NAME, status);
+	fmt.Println(selectQuery);
+	entries, err := tx.Query(selectQuery)
 	if (err != nil) {
-		log.Fatal("Failed to clean uo update price request table")
+		log.Fatalf("Query failed while selecting update request \n err: %v", err)
 	}
+	return entries;
 }
+
+func ChangeStatusFrom(oldStatus string, newStatus string, tx sql.Tx) error {
+	updateQuery := fmt.Sprintf("update %s.%s set status = '%s' where status = '%s'", SCHEMA_NAME, PRICE_UPDATE_REQUEST_TABLE_NAME, newStatus, oldStatus)
+
+	_, err := tx.Exec(updateQuery)
+	return err;
+}
+
