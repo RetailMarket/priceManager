b248a13364188506703e886a0e6f3aa5502d0b0d
Akshay <kumarsharmaa69@gmail.com>
2017-01-24 10:14:22 +0530

    Migration in price and price update request table

--- db/migrations/20170119121236_PriceUpdateRequest.sql
+++ db/migrations/20170119121236_PriceUpdateRequest.sql
@@ -1,15 +1,17 @@
 
 -- +goose Up
-CREATE TABLE UpdatePriceRequest (
+CREATE TABLE PriceUpdateRequest (
   product_id 				  int not null,
   product_name			  text not null,
   cost				        float not null,
-  status			        text not null
+  status			        text not null,
+  created_at   TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at   TIMESTAMP NOT NULL DEFAULT now()
 );
 -- SQL in section 'Up' is executed when this migration is applied
 
 
 -- +goose Down
-DROP TABLE UpdatePriceRequest;
+DROP TABLE PriceUpdateRequest;
 -- SQL section 'Down' is executed when this migration is rolled back
 
--- db/migrations/20170119145423_Price.sql
+++ db/migrations/20170119145423_Price.sql
@@ -1,9 +1,10 @@
-
 -- +goose Up
 CREATE TABLE Price (
-  product_id 				  int not null,
-  product_name	  		text not null,
-  cost				        float not null
+  product_id   INT       NOT NULL,
+  product_name TEXT      NOT NULL,
+  cost         FLOAT     NOT NULL,
+  created_at   TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at   TIMESTAMP NOT NULL DEFAULT now()
 );
 -- SQL in section 'Up' is executed when this migration is applied
 
