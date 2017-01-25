797a7db5f9d3b7189eea669e63fe3980de7c68c6
Akshay <kumarsharmaa69@gmail.com>
2017-01-25 16:16:24 +0530

    Modified price table columns

--- db/migrations/20170119145423_Price.sql
+++ db/migrations/20170119145423_Price.sql
@@ -3,8 +3,8 @@ CREATE TABLE Price (
   product_id   INT       NOT NULL,
   product_name TEXT      NOT NULL,
   cost         FLOAT     NOT NULL,
-  created_at   TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at   TIMESTAMP NOT NULL DEFAULT now()
+  version      TEXT      NOT NULL,
+  is_latest    BOOLEAN   NOT NULL
 );
 -- SQL in section 'Up' is executed when this migration is applied
 
