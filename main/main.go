4047d10490af657a9d234d88338ce001b097305c
Akshay <kumarsharmaa69@gmail.com>
2017-01-25 16:28:42 +0530

    Removed job calling and workflow clinet connection from main

--- main/main.go
+++ main/main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"Retail/priceManager/database"
-	"Retail/priceManager/client"
 	"Retail/priceManager/seeds"
-	"Retail/priceManager/jobs"
-	_"Retail/priceManager/routes"
+	"Retail/priceManager/server"
 )
 
 func main() {
 	database.Init();
 	defer database.CloseDb();
-
-	// seeding data into DB.
 	seeds.UploadSeedForPriceTable();
-	seeds.UploadSeedForPriceUpdateRequestTable();
 
-	workflowClient, conn := client.CreateClientConnection();
-
-	// closing client connection.
-	defer conn.Close();
-
-	// running job for sending update price record for approval.
-	jobs.SendUpdatePriceForApprovalJob(workflowClient)
-	//routes.HandleRequest()
+	server.CreateServerConnection();
 }
