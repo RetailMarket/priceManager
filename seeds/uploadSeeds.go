981f64992131862704593fdd87cd2181059853d9
Akshay <kumarsharmaa69@gmail.com>
2017-01-25 16:21:01 +0530

    Modified seed data

--- seeds/uploadSeeds.go
+++ seeds/uploadSeeds.go
@@ -10,6 +10,8 @@ type Product struct {
 	product_id   int
 	product_name string
 	cost         int
+	version      string
+	is_latest    bool
 	status       string
 }
 
@@ -23,14 +25,28 @@ func UploadSeedForPriceTable() {
 
 	var config Config
 
-	p1 := Product{product_id: 1001, product_name: "Pen", cost: 10}
-	p2 := Product{product_id: 1002, product_name: "Pencil", cost: 4}
-	p3 := Product{product_id: 1003, product_name: "Rubber", cost: 2}
-	p4 := Product{product_id: 1004, product_name: "Sticky", cost: 10}
-	p5 := Product{product_id: 1005, product_name: "chart", cost: 5}
-	p6 := Product{product_id: 1006, product_name: "global map", cost: 2}
+	p1 := Product{product_id: 1001, product_name: "Pen", cost: 10, version:"v1", is_latest:false}
+	p2 := Product{product_id: 1001, product_name: "Pen", cost: 12, version:"v2", is_latest:true}
+	p3 := Product{product_id: 1001, product_name: "Pen", cost: 14, version:"v3", is_latest:false}
 
-	config.Products = append(config.Products, p1, p2, p3, p4, p5, p6)
+	p4 := Product{product_id: 1002, product_name: "Pencil", cost: 4, version:"v1", is_latest:true}
+	p5 := Product{product_id: 1002, product_name: "Pencil", cost: 6, version:"v2", is_latest:false}
+
+	p6 := Product{product_id: 1003, product_name: "Rubber", cost: 2, version:"v1", is_latest:false}
+	p7 := Product{product_id: 1003, product_name: "Rubber", cost: 3, version:"v2", is_latest:false}
+	p8 := Product{product_id: 1003, product_name: "Rubber", cost: 5, version:"v3", is_latest:true}
+	p9 := Product{product_id: 1003, product_name: "Rubber", cost: 8, version:"v4", is_latest:false}
+
+	p10 := Product{product_id: 1004, product_name: "Sticky", cost: 10, version:"v1", is_latest:false}
+	p11 := Product{product_id: 1004, product_name: "Sticky", cost: 13, version:"v2", is_latest:true}
+
+	p12 := Product{product_id: 1005, product_name: "chart", cost: 5, version:"v1", is_latest:true}
+
+	p13 := Product{product_id: 1006, product_name: "global map", cost: 2, version:"v1", is_latest:false}
+	p14 := Product{product_id: 1006, product_name: "global map", cost: 3, version:"v2", is_latest:true}
+	p15 := Product{product_id: 1006, product_name: "global map", cost: 4, version:"v3", is_latest:false}
+
+	config.Products = append(config.Products, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15)
 
 	_, err := db.Exec("truncate table price.price")
 	if (err != nil) {
@@ -39,41 +55,11 @@ func UploadSeedForPriceTable() {
 
 	for i := 0; i < len(config.Products); i++ {
 		product := config.Products[i];
-		_, err := db.Exec("insert into price.price (product_id,product_name,cost) values ($1,$2,$3)", product.product_id, product.product_name, product.cost)
+		_, err := db.Exec("insert into price.price (product_id,product_name,cost,version, is_latest) values ($1,$2,$3, $4,$5)", product.product_id, product.product_name, product.cost, product.version, product.is_latest)
 		if (err != nil) {
 			panic(err)
 		}
 	}
 	fmt.Println("Seed successful for price table :)")
-}
-
-func UploadSeedForPriceUpdateRequestTable() {
-	db := database.GetDb()
-	fmt.Println("Seeding data to price update table...")
-
-	var config Config
-
-	p1 := Product{product_id: 1001, product_name: "Pen", cost: 12, status: status.PENDING}
-	p2 := Product{product_id: 1002, product_name: "Pencil", cost: 5, status: status.PENDING}
-	p3 := Product{product_id: 1003, product_name: "Rubber", cost: 3, status: status.PENDING}
-	p4 := Product{product_id: 1004, product_name: "Sticky", cost: 20, status: status.COMPLETED}
-	p5 := Product{product_id: 1005, product_name: "chart", cost: 10, status: status.COMPLETED}
-
-	config.Products = append(config.Products, p1, p2, p3, p4, p5)
-
-	_, err := db.Exec("truncate table price.priceUpdateRequest")
-	if (err != nil) {
-		panic(err.Error())
-	}
-
-	for i := 0; i < len(config.Products); i++ {
-		product := config.Products[i];
-		_, err := db.Exec("insert into price.priceUpdateRequest (product_id,product_name,cost,status) values ($1,$2,$3,$4)", product.product_id, product.product_name, product.cost, product.status)
-		if (err != nil) {
-			panic(err)
-		}
-
-	}
-	fmt.Println("Seed successful for price update table :)")
 }
 
