888c5d983805c63f660adae7f96a104dd0289594
Akshay <kumarsharmaa69@gmail.com>
2017-01-25 18:09:35 +0530

    Added status in price table

--- db/migrations/20170119145423_Price.sql
+++ db/migrations/20170119145423_Price.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 CREATE TABLE Price (
-  product_id   INT       NOT NULL,
-  product_name TEXT      NOT NULL,
-  cost         FLOAT     NOT NULL,
-  version      TEXT      NOT NULL,
-  is_latest    BOOLEAN   NOT NULL
+  product_id   INT     NOT NULL,
+  product_name TEXT    NOT NULL,
+  cost         FLOAT   NOT NULL,
+  version      TEXT    NOT NULL,
+  status       TEXT    NOT NULL,
+  is_latest    BOOLEAN NOT NULL
 );
 -- SQL in section 'Up' is executed when this migration is applied
 
